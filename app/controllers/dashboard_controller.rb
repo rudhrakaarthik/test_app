e585a618e7ae6a212144d6b6f1e4702206ea584b
Karthik Marimuthu <karthikm2010@gmail.com>
2015-01-21 17:31:56 +0530

    tweets functionality completed

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,24 @@
 class DashboardController < ApplicationController
+
   def index
+  end
+
+  def show
+    @client_details = client.user_timeline("#{params["user_name"]}").first(10).map{|tweet| tweet.text}
+  rescue => e
+    @error = "Error while fetching tweets for: #{params["user_name"]}, #{e.message} "
+    Rails.logger.error {@error}
+    nil
+  end
+
+  private
+
+  def client
+    @client ||= Twitter::REST::Client.new do |config|
+      config.consumer_key = Rails.application.secrets.CONSUMER_KEY
+      config.consumer_secret = Rails.application.secrets.CONSUMER_SECRET
+      config.access_token = Rails.application.secrets.ACCESS_TOKEN
+      config.access_token_secret = Rails.application.secrets.ACCESS_SECRET
+    end
   end
 end
